9fc6c32329432330f5f11f8384ce834cd8fe3ce3
mnvro <mnvro@hotmail.com>
2019-01-26 13:58:12 +0100

    Cercle avec la méthode agrandit en plus

--- cm2019/src/cours02/Cercle.java
+++ cm2019/src/cours02/Cercle.java
@@ -4,16 +4,25 @@ public class Cercle {
 	private Point centre;
 	private double rayon;
 	
+	public void agrandit(double facteur) {
+		rayon *= facteur;
+	}
+	
 	public Cercle(Point centre, double rayon) {
 		this.centre = centre;
 		this.rayon = rayon;
 	}
-
+	public String toString() {
+		String s = "Cercle de centre "+centre.toString()+" et de rayon "+rayon+ " mètres";
+		return s;
+	}
 	public Cercle() {
 	}
 	public static void main(String[] args) {
 		Cercle cercle1 = new Cercle(new Point('C',2,2), 5);
-		
+		System.out.println(cercle1);
+		cercle1.agrandit(0.5);
+		System.out.println(cercle1);
 	}
 
 
